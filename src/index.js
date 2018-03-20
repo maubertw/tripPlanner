bba4c62bc9bc4e8bd36c851e7e59e2ee2702e2d3
SamKF <samsonkfanuel@gmail.com>
2018-03-20 13:12:33 -0500

    add buildMarker

--- src/index.js
+++ src/index.js
@@ -6,20 +6,23 @@ const mapboxgl = require("mapbox-gl");
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyeS13YXJyaWNrIiwiYSI6ImNqZXp0cnp4NzBlMXIyd29heHppM2R1ODQifQ.V4xaaHhhwf4cempiwI45VQ';
 
-const map = new mapboxgl.Map({
-  container: "map",
-  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
-});
+// const map = new mapboxgl.Map({
+//   container: "map",
+//   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+//   zoom: 12, // starting zoom
+//   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+// });
 
-let markerElement = document.createElement("div");
-markerElement.style.width = "32px";
-markerElement.style.height = "39px";
-markerElement.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+// let markerElement = document.createElement("div");
+// markerElement.style.width = "32px";
+// markerElement.style.height = "39px";
+// markerElement.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
 
 
 let flag = new mapboxgl.Marker(markerElement).setLngLat([-87.6354, 41.8885]).addTo(map);
 
 const marker = buildMarker("activities", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+
 marker.addTo(map);
+
+// marker(buildMarker.iconURLs.hotels, [-87.6354, 41.8885]);
--- src/marker.js
+++ src/marker.js
@@ -1,4 +1,7 @@
-const mapbox = require("mapbox-gl");
+//const mapbox = require("mapbox-gl");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFyeS13YXJyaWNrIiwiYSI6ImNqZXp0cnp4NzBlMXIyd29heHppM2R1ODQifQ.V4xaaHhhwf4cempiwI45VQ';
 
 const iconURLs = {
     hotels: "http://i.imgur.com/D9574Cu.png",
@@ -10,7 +13,7 @@ const buildMarker = function(type, cords) {
     let markerElement = document.createElement("div");
     markerElement.style.width = "32px";
     markerElement.style.height = "39px";
-    markerElement.style.backgroundImage = "url(" + type + ")";
+    markerElement.style.backgroundImage = "url(" + iconURLs[type] + ")";
 
 
     return new mapboxgl.Marker(markerElement).setLngLat(cords)
